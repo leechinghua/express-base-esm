d510049d6911ec79805a0ae9ff0cb19ced253fef
leechinghua <leechenghua107@gmail.com>
2024-07-09 19:21:30 +0800

    新增customer名單跟路由頁面

--- routes/welcome.js
+++ routes/welcome.js
@@ -6,24 +6,27 @@ import db from '#configs/mysql.js'
 
 // GET - 得到所有資料
 router.get('/', async function (req, res) {
-  const products = rows
+  const [rows] = await db.query('SELECT * FROM customer')
+  const customers = rows
 
   // 標準回傳JSON
   return res.json({
     status: 'success',
-    data: {},
+    data: { customers },
   })
 })
 
 // GET - 得到單筆資料(注意，有動態參數時要寫在GET區段最後面)
-// router.get('/:id', async function (req, res) {
-//   // 轉為數字
-//   const id = Number(req.params.id)
+router.get('/:id', async function (req, res) {
+  const id = Number(req.params.id)
+  const [rows] = await db.query('SELECT * FROM customer WHERE id = ?', [id])
+  const customer = rows[0]
 
-//   const [rows] = await db.query('SELECT * FROM my_product WHERE id = ?', [id])
-//   const product = rows[0]
-
-//   return res.json({ status: 'success', data: { product } })
-// })
+  // 標準回傳JSON
+  return res.json({
+    status: 'success',
+    data: { customer },
+  })
+})
 
 export default router
